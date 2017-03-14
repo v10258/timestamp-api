41c59035f835b18af2502011c007ee839a6e5ab9
niefeng <v10258@qq.com>
2017-03-14 02:41:08 +0000

    finish api

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
+function getClientIp(req) {
+    return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+};
+
 app.get('/', (req, res)=>{
-    res.send('Hello world')
+    var now = new Date();
+    var result = {};
+    
+    result.ipaddress = getClientIp(req);
+    result.language = req.headers['accept-language'].split(',')[0];
+    result.software = req.headers['user-agent'].match(/\(([^\)]+)\)/)[1];
+    
+    res.send(JSON.stringify(result));
 });
 
 app.listen(8080);
